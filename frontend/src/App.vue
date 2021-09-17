c7d68f57f78b586416c78aee305b4d8ad951951d
simonteozw <simonteozw@gmail.com>
2021-09-17 12:52:20 +0800

    Set up basic UI

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,34 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <div class="row">
+      <div class="col -md-6 offset-md-3 py-5">
+        <h1>Generate thumbnail of a website</h1>
+
+        <form v-on:submit.prevent="makeWebsiteThumbnail">
+          <div class="form-group">
+            <input v-model="websiteUrl" type="text" id="website-input" placeholder="Enter a website" class="form-control">
+          </div>
+          <div class="form-group">
+            <button class="btn btn-primary">Generate!</button>
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+
+  data() { return {
+    websiteUrl: '',
+  } },
+
+  methods: {
+    makeWebsiteThumbnail() {
+      console.log(`${this.websiteUrl}`);
+    }
   }
 }
 </script>
